bbfc1e93c79a257e32dd0b38b97998deef08997a
Lars Bohm <larsbohm@gmail.com>
2017-10-03 16:04:59 +0200

    first version routes

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,3 +1,7 @@
 class Blog < ApplicationRecord
   belongs_to :user
+
+  def to_param
+    publishing_date.strftime('%d-%m-%Y')
+  end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,2 +1,6 @@
 class Project < ApplicationRecord
+
+  def to_param
+    title.downcase.gsub(/\W/, "-")
+  end
 end
