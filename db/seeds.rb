2c8b820e97e73041bfef77833191cac9536ec32d
Lars Bohm <larsbohm@gmail.com>
2017-11-14 17:13:37 +0100

    heel veel dingen, weet niet meer precies wat allemaal. Maar erg belangrijk

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Page.destroy_all
-Section.destroy_all
-Header.destroy_all
-TextArea.destroy_all
 
-page = Page.create(identifier: "home")
+# TODO maybe remove or rework the admin section
 
-section_1 = Section.create(page: page, identifier: "left")
+# Page.destroy_all
+# Section.destroy_all
+# Header.destroy_all
+# TextArea.destroy_all
 
-Header.create(identifier: "header-left", section: section_1, content: "Build it and they will come")
+# page = Page.create(identifier: "home")
 
-section_2 = Section.create(page: page, identifier: "right")
+# section_1 = Section.create(page: page, identifier: "left")
 
-TextArea.create(identifier: "main-1", section: section_2, content: "We're going to get straight to the point; we like to build stuff. What kind of stuff? Places where people can connect, where they can gather. Places where people can find what they're looking for.")
-TextArea.create(identifier: "main-2", section: section_2, content: "We love a challenge. Find us a group of people and a problem and we'll help you find, not only a great, but a beautiful solution. ")
+# Header.create(identifier: "header-left", section: section_1, content: "Build it and they will come")
+
+# section_2 = Section.create(page: page, identifier: "right")
+
+# TextArea.create(identifier: "main-1", section: section_2, content: "We're going to get straight to the point; we like to build stuff. What kind of stuff? Places where people can connect, where they can gather. Places where people can find what they're looking for.")
+# TextArea.create(identifier: "main-2", section: section_2, content: "We love a challenge. Find us a group of people and a problem and we'll help you find, not only a great, but a beautiful solution. ")
 
