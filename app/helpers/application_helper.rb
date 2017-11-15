0c9cb0a2f67d9b6c82876c295973090a6e16ab0d
michele de bruyn <debruyn.mm@gmail.com>
2017-11-15 14:08:31 +0100

    Removed secondary navbar and all methods & views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,46 +15,7 @@ module ApplicationHelper
     Page.find_by(identifier: identifier)
   end
 
-
   def is_home?
     return true if params[:controller] == 'pages' && params[:action] == 'home'
-  end
-
-  def secondary_nav_items
-    {
-    "services" =>
-      {
-        "MVP" => mvp_services_url,
-        "Workshops" => workshops_services_url,
-        "Sprints" => sprints_services_url
-      },
-    # "portfolios" =>
-    #   {
-    #     "Projects" => portfolio_projects_url,
-    #     "Partnerships" => portfolio_partnerships_url
-    #   },
-    "abouts" =>
-      {
-        "Blog" => about_blog_index_url,
-        "Method" => method_about_url,
-        "Contact" => contact_url
-      },
-      "projects" =>
-      {
-        "Projects" => portfolio_projects_url,
-        "Partnerships" => portfolio_partnerships_url
-      },
-      "partnerships" =>
-      {
-        "Projects" => portfolio_projects_url,
-        "Partnerships" => portfolio_partnerships_url
-      },
-      "blog" =>
-      {
-        "Blog" => about_blog_index_url,
-        "Method" => method_about_url,
-        "Contact" => contact_url
-      }
-    }
   end
 end
