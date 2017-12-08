00cd394b5afdf1988df84f6ae777cca8de065b86
michele de bruyn <debruyn.mm@gmail.com>
2017-12-08 20:37:21 +0100

    remove more admin logic

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,5 @@
 class PagesController < ApplicationController
   def home
-    page = helpers.set_page('home')
-    @left = helpers.set_section(page, "left")
-    @right = helpers.set_section(page, "right")
   end
 
   def contact
