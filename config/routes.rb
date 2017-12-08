ab66ab1f2494499b8440a41bc8d096e26198a040
michele de bruyn <debruyn.mm@gmail.com>
2017-12-08 20:29:56 +0100

    maybe fix a controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,36 +1,25 @@
 Rails.application.routes.draw do
 
-  namespace :admin do
-    devise_for :users
-    resource :dashboard
+  # namespace :admin do
+  #   devise_for :users
+  #   resource :dashboard
 
-    resources :projects, except: [:show]
-    resources :services, except: [:show]
-    resources :blogs, except: [:show]
-    resources :abouts, except: [:show]
+  #   resources :projects, except: [:show]
+  #   resources :services, except: [:show]
+  #   resources :blogs, except: [:show]
+  #   resources :abouts, except: [:show]
 
-  end
+  # end
 
 
   root to: 'pages#home'
   get '/contact', to: 'pages#contact'
-  resource :services, only: [:show] do
-    get 'mvp', to: 'services#mvp'
-    get 'digitalconsulting', to: 'services#digitalconsulting'
-    get 'sprints', to: 'services#sprints'
-  end
+  resource :services, only: [:show]
 
   resource :portfolio, only: [:show] do
-    resources :projects, only: [:index]
     get '/projects/:title', to: 'projects#show', as: :project
-    resources :partnerships, only: [:index]
   end
 
-  resource :about, only: [:show] do
-    resources :blog, only: [:index]
-    get '/blog/:publishing_date', to: 'blog#show'
-    get 'method', to: 'abouts#workmethod'
-  end
-
+  resource :about, only: [:show]
 
 end
