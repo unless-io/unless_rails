5dc55c76c5ee50cd4876bf6265d1aa1db9ddb812
Lars Bohm <larsbohm@gmail.com>
2018-01-16 15:43:48 +0100

    add extra information page with pricing info

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,7 @@ class PagesController < ApplicationController
 
   def contact
   end
+
+  def extra_information
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
   get '/contact', to: 'pages#contact'
+  get '/extra-information', to: 'pages#extra_information'
+
   resource :services, only: [:show]
 
   resource :portfolio, only: [:show] do
